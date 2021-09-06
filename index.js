86248398406d7144c86bfae7b50baaa60c427423
Carlos Sevilla <sevillecarlos@gmail.com>
2021-09-05 19:19:37 -0600

    add image extension filter

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-
 require("dotenv").config();
 require("./config/db");
-
 
 const auth = require("./routes/api/auth");
 const userDataGame = require("./routes/api/userGameData");
 
-const PORT = 5000 | process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(auth).use(userDataGame);
-app.use('/upload', express.static('upload')); 
+app.use("/upload", express.static("upload"));
 
 /////////////////////////////////////////////////////
 app.get("/", (req, res) => {
-  res.send("Express Server Running...");
+  res.send("Server Express Up");
 });
 
 app.listen(PORT, (err) => {
